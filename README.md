cb6a3a896a8ed111f00bde522414ddf6047fc544
hqjs <hqjsorg@gmail.com>
2019-07-22 17:15:21 +0200

    Improve destructuring for types

--- README.md
+++ README.md
@@ -5,3 +5,105 @@ Transform named import to destructure
 ```sh
 npm install hqjs@babel-plugin-transform-named-import-to-destructure
 ```
+
+# Transformation
+Plugin makes destructure imports work with circular dependencies, it also work together with typescript and type metadata.
+```ts
+...
+import { Injectable } from '@angular/core';
+
+import { Observable, of } from 'rxjs'; // Observable is only type import, while of is regular import
+
+import { Hero } from './hero'; // That is only type import
+import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service'; // This is type import, but it is required for metadata
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HeroService {
+
+  constructor(private messageService: MessageService) { }
+
+  getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
+  }
+}
+
+```
+
+will turn into
+```ts
+import * as _ref from "@angular/core";
+
+let Injectable, _ref2;
+
+try {
+  _ref2 = Object.keys(_ref).length === 1 && _ref.default ? _ref.default : _ref;
+  Injectable = _ref2.Injectable;
+} catch {
+  Promise.resolve().then(() => {
+    _ref2 = Object.keys(_ref).length === 1 && _ref.default ? _ref.default : _ref;
+    Injectable = _ref2.Injectable;
+  }).catch(() => console.error("Unable to resolve cyclic dependencies between module \"./hero.service.ts.map*\" and \"@angular/core.map*\" while requesting \"Injectable\". Try to change imports order in a parent module"));
+}
+
+import * as _ref3 from "rxjs";
+
+let of, _ref4;
+
+try {
+  _ref4 = Object.keys(_ref3).length === 1 && _ref3.default ? _ref3.default : _ref3;
+  of = _ref4.of;
+} catch {
+  Promise.resolve().then(() => {
+    _ref4 = Object.keys(_ref3).length === 1 && _ref3.default ? _ref3.default : _ref3;
+    of = _ref4.of;
+  }).catch(() => console.error("Unable to resolve cyclic dependencies between module \"./hero.service.ts.map*\" and \"rxjs.map*\" while requesting \"of\". Try to change imports order in a parent module"));
+}
+
+import * as _ref5 from "./mock-heroes";
+
+let HEROES, _ref6;
+
+try {
+  _ref6 = Object.keys(_ref5).length === 1 && _ref5.default ? _ref5.default : _ref5;
+  HEROES = _ref6.HEROES;
+} catch {
+  Promise.resolve().then(() => {
+    _ref6 = Object.keys(_ref5).length === 1 && _ref5.default ? _ref5.default : _ref5;
+    HEROES = _ref6.HEROES;
+  }).catch(() => console.error("Unable to resolve cyclic dependencies between module \"./hero.service.ts.map*\" and \"./mock-heroes.map*\" while requesting \"HEROES\". Try to change imports order in a parent module"));
+}
+
+import * as _ref7 from "./message.service";
+
+let MessageService, _ref8;
+
+try {
+  _ref8 = Object.keys(_ref7).length === 1 && _ref7.default ? _ref7.default : _ref7;
+  MessageService = _ref8.MessageService;
+} catch {
+  Promise.resolve().then(() => {
+    _ref8 = Object.keys(_ref7).length === 1 && _ref7.default ? _ref7.default : _ref7;
+    MessageService = _ref8.MessageService;
+  }).catch(() => console.error("Unable to resolve cyclic dependencies between module \"./hero.service.ts.map*\" and \"./message.service.map*\" while requesting \"MessageService\". Try to change imports order in a parent module"));
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HeroService {
+
+  constructor(private messageService: MessageService) { }
+
+  getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
+  }
+}
+
+```
+
+that works nice with decorator plugin.
--- index.js
+++ index.js
@@ -1,4 +1,48 @@
-module.exports = function({ types: t }) {
+const jsxPragma = 'React';
+let fileJsxPragma = '';
+let sourceFileHasJsx = false;
+const JSX_ANNOTATION_REGEX = /\*?\s*@jsx\s+([^\s]+)/;
+
+function notConstructorParameterType(nodePath) {
+  const paramPropChild = nodePath.parentPath &&
+    nodePath.parentPath.parentPath &&
+    nodePath.parentPath.parentPath.parentPath
+  if (!paramPropChild || !paramPropChild.parent) return true;
+  const isParamProp = paramPropChild.parent.type === 'TSParameterProperty';
+  if (!isParamProp) return true;
+  const classMethod = paramPropChild.parentPath.parent && paramPropChild.parentPath.parent;
+  if (!classMethod) return true;
+  return !(classMethod.type === 'ClassMethod' && classMethod.kind === 'constructor');
+}
+
+function isInType(nodePath) {
+  switch (nodePath.parent.type) {
+    case 'TSTypeReference':
+    case 'TSQualifiedName':
+    case 'TSExpressionWithTypeArguments':
+    case 'TSTypeQuery':
+      return notConstructorParameterType(nodePath);
+    default:
+      return false;
+  }
+}
+
+function isImportTypeOnly({ binding, jsxPragma }) {
+  for (const nodePath of binding.referencePaths) {
+    if (!isInType(nodePath)) {
+      return false;
+    }
+  }
+
+  if (binding.identifier.name !== jsxPragma) {
+    return true;
+  }
+
+  // "React" or the JSX pragma is referenced as a value if there are any JSX elements in the code.
+  return !sourceFileHasJsx;
+}
+
+module.exports = function ({ types: t }) {
   const objectKeys = t.memberExpression(
     t.identifier('Object'),
     t.identifier('keys')
@@ -7,61 +51,91 @@ module.exports = function({ types: t }) {
   const one = t.numericLiteral(1);
   return {
     visitor: {
-      ImportDeclaration(nodePath, state) {
-        const { baseURI = '', map = '' } = state.opts;
-        const { filename = '' } = state.file.opts;
-        const { specifiers, source } = nodePath.node;
-        if (specifiers.length === 0) return;
-        const name = nodePath.scope.generateUidIdentifierBasedOnNode('destructure');
-        const exportDefault = t.memberExpression(
-          name,
-          t.identifier('default')
-        );
-        const modName = nodePath.scope.generateUidIdentifierBasedOnNode('destructure');
-        const dest = t.variableDeclaration(
-          'let',
-          specifiers.map(({ local }) => t.variableDeclarator(local)).concat(t.variableDeclarator(modName))
-        );
-        const importAssignment = t.blockStatement([
-          t.expressionStatement(t.assignmentExpression(
-            '=',
-            modName,
-            t.conditionalExpression(
-              t.logicalExpression(
-                '&&',
-                t.binaryExpression(
-                  '===',
-                  t.memberExpression(
-                    t.callExpression(
-                      objectKeys,
-                      [ name ]
+      Program(nodePath, state) {
+        const { file } = state;
+
+        if (file.ast.comments) {
+          for (const comment of (file.ast.comments)) {
+            const jsxMatches = JSX_ANNOTATION_REGEX.exec(comment.value);
+            if (jsxMatches) {
+              fileJsxPragma = jsxMatches[1];
+            }
+          }
+        }
+        nodePath.traverse({
+          JSXElement() {
+            sourceFileHasJsx = true;
+          },
+          JSXFragment() {
+            sourceFileHasJsx = true;
+          },
+        });
+      },
+      ImportDeclaration: {
+        exit(nodePath, state) {
+          const { baseURI = '', map = '' } = state.opts;
+          const { filename = '' } = state.file.opts;
+          const { specifiers, source } = nodePath.node;
+          const importSpecifiers = specifiers.filter(({ local }) => {
+            const binding = nodePath.scope.getBinding(local.name);
+            return !binding ||
+              !isImportTypeOnly({
+                binding,
+                jsxPragma: fileJsxPragma || jsxPragma,
+              });
+          });
+
+          if (importSpecifiers.length === 0) return;
+          const name = nodePath.scope.generateUidIdentifierBasedOnNode('destructure');
+          const exportDefault = t.memberExpression(
+            name,
+            t.identifier('default')
+          );
+          const modName = nodePath.scope.generateUidIdentifierBasedOnNode('destructure');
+          const dest = t.variableDeclaration(
+            'let',
+            importSpecifiers.map(({ local }) => t.variableDeclarator(local)).concat(t.variableDeclarator(modName))
+          );
+          const importAssignment = t.blockStatement([
+            t.expressionStatement(t.assignmentExpression(
+              '=',
+              modName,
+              t.conditionalExpression(
+                t.logicalExpression(
+                  '&&',
+                  t.binaryExpression(
+                    '===',
+                    t.memberExpression(
+                      t.callExpression(
+                        objectKeys,
+                        [name]
+                      ),
+                      length
                     ),
-                    length
+                    one
                   ),
-                  one
+                  exportDefault
                 ),
-                exportDefault
-              ),
-              exportDefault,
-              name
-            )
-          )),
-          ...specifiers.map(spec => t.expressionStatement(t.assignmentExpression(
-            '=',
-            spec.local,
-            spec.imported ?
-            	t.memberExpression(modName, spec.imported) :
-            	t.isImportNamespaceSpecifier(spec) ?
-            		name :
-            		modName
-          ))),
-        ]);
-        const decl = t.tryStatement(
-          importAssignment,
-          t.catchClause(
-            null,
-            t.blockStatement([
-              t.expressionStatement(t.callExpression(
+                exportDefault,
+                name
+              )
+            )),
+            ...importSpecifiers.map(spec => t.expressionStatement(t.assignmentExpression(
+              '=',
+              spec.local,
+              spec.imported ?
+                t.memberExpression(modName, spec.imported) :
+                t.isImportNamespaceSpecifier(spec) ?
+                  name :
+                  modName
+            ))),
+          ]);
+          const decl = t.tryStatement(
+            importAssignment,
+            t.catchClause(
+              null,
+              t.blockStatement([
+                t.expressionStatement(t.callExpression(
                   t.memberExpression(
                     t.callExpression(
                       t.memberExpression(
@@ -91,17 +165,18 @@ module.exports = function({ types: t }) {
                           t.identifier('console'),
                           t.identifier('error')
                         ),
-                        [t.stringLiteral(`Unable to resolve cyclic dependencies between module "${baseURI}${filename}${map}" and "${source.value}${map}" while requesting "${specifiers.map(s => s.imported ? s.imported.name : t.isImportNamespaceSpecifier(s) ? '*' : 'default').join('", ')}". Try to change imports order in a parent module`)]
+                        [t.stringLiteral(`Unable to resolve cyclic dependencies between module "${baseURI}${filename}${map}" and "${source.value}${map}" while requesting "${importSpecifiers.map(s => s.imported ? s.imported.name : t.isImportNamespaceSpecifier(s) ? '*' : 'default').join('", ')}". Try to change imports order in a parent module`)]
                       ),
                     ),
                   ]
                 )),
-            ])
-          )
-        );
-        nodePath.node.specifiers = [ t.ImportNamespaceSpecifier(name) ];
-        nodePath.insertAfter(decl);
-        nodePath.insertAfter(dest);
+              ])
+            )
+          );
+          nodePath.node.specifiers = [t.ImportNamespaceSpecifier(name)];
+          nodePath.insertAfter(decl);
+          nodePath.insertAfter(dest);
+        }
       },
     },
   };
